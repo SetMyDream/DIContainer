e2e6ccc000871891cd891d53115239e7504c4f8c
Artem <artem.vorobiov@sigma.software>
2023-06-29 18:54:22 +0300

    added isolation test

---
+++ src/main/test/DependencyInjectionContainerTest.java
@@ -0,0 +1,26 @@
+package main.test;
+
+import main.DependencyInjectionContainer;
+import main.components.ExampleComponent;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertNotSame;
+
+public class DependencyInjectionContainerTest {
+
+    @Test
+    public void testContainerIsolation() throws Exception {
+        DependencyInjectionContainer container1 = new DependencyInjectionContainer();
+        container1.scanAndRegisterComponents("main.components");
+        container1.initialize();
+
+        DependencyInjectionContainer container2 = new DependencyInjectionContainer();
+        container2.scanAndRegisterComponents("main.components");
+        container2.initialize();
+
+        ExampleComponent exampleComponent1 = container1.getInstance(ExampleComponent.class);
+        ExampleComponent exampleComponent2 = container2.getInstance(ExampleComponent.class);
+
+        assertNotSame(exampleComponent1, exampleComponent2);
+    }
+}
