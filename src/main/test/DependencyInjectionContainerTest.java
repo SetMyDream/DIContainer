9e6ee9abcd3aacac3cd98ca5d26b0b43e752e5f1
Artem <artem.vorobiov@sigma.software>
2023-06-30 13:37:35 +0300

    positive tests for @Container and Autowired

--- src/main/test/DependencyInjectionContainerTest.java
+++ src/main/test/DependencyInjectionContainerTest.java
@@ -1,13 +1,22 @@
 package main.test;
 
+import main.components.AnotherComponent;
 import main.container.DependencyInjectionContainer;
 import main.components.ExampleComponent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertNotSame;
+import static org.junit.Assert.assertNotNull;
 
 public class DependencyInjectionContainerTest {
 
+    private DependencyInjectionContainer container;
+
+    @BeforeEach
+    public void setUp() {
+        container = new DependencyInjectionContainer();
+    }
 
     //Контейнер должен быть изолированным.
     @Test
@@ -24,5 +33,32 @@ public class DependencyInjectionContainerTest {
         ExampleComponent exampleComponent2 = container2.getInstance(ExampleComponent.class);
 
         assertNotSame(exampleComponent1, exampleComponent2);
+    }
+
+    @Test
+    public void testExampleComponent() {
+        try {
+            container.register(ExampleComponent.class);
+            container.register(AnotherComponent.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ExampleComponent exampleComponent = container.getInstance(ExampleComponent.class);
+        assertNotNull(exampleComponent);
+
+        exampleComponent.doSomething();
+    }
+
+    @Test
+    public void testAnotherComponent() {
+        try {
+            container.register(AnotherComponent.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        AnotherComponent anotherComponent = container.getInstance(AnotherComponent.class);
+        assertNotNull(anotherComponent);
     }
 }
